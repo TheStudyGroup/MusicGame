83a319b7a2f7256a119b5b31a69801cc4fd430c2
Deun Lee <54697735+deunlee@users.noreply.github.com>
2021-05-20 22:10:22 +0900

    Fix timer function

--- Sources/Timer/Timer.h
+++ Sources/Timer/Timer.h
@@ -1,14 +1,12 @@
 #ifndef _Timer_H_
 #define _Timer_H_
 
-#include "LPC1768_utility.h"
-#include "lpc17xx_timer.h"
+#include "../Utility/Utility.h"
+
+void DelayMs(uint32_t millis);
 
 void Timer0_Init(void);
 void Timer0_Start(void);
 void Timer0_Stop(void);
-
-void __Timer0_Config(void);
-void TIMER0_IRQHandler(void);
 
 #endif
