18d7a6856840521e0e802d94e4124fcc162f8dd3
Deun Lee <54697735+deunlee@users.noreply.github.com>
2021-05-25 00:35:54 +0900

    Fix tone functions

--- Source/Tone.c
+++ Source/Tone.c
@@ -7,15 +7,15 @@
 
 #define TONE_SAMPLE_COUNT 512
 
-uint8_t                Tone_Volume = 50;   // 0~100
-bool                   Tone_State  = false; // play state
-uint32_t               dacSamples[TONE_SAMPLE_COUNT]; // 2 KB
+uint32_t Tone_State = 0;  // play state
+uint32_t Samples_Sine[TONE_SAMPLE_COUNT]; // 2 KB
+uint32_t Samples_DAC [TONE_SAMPLE_COUNT]; // 2 KB
 GPDMA_LLI_Type         dmaLinkListItem;
 GPDMA_Channel_CFG_Type gpdmaConfig;
 
 static void Tone_DmaInit(void) {
     // Prepare DMA Link-List-Item structure
-    dmaLinkListItem.SrcAddr = (uint32_t)dacSamples;
+    dmaLinkListItem.SrcAddr = (uint32_t)Samples_DAC;
     dmaLinkListItem.DstAddr = (uint32_t)&(LPC_DAC->DACR);
     dmaLinkListItem.NextLLI = (uint32_t)&dmaLinkListItem;
     dmaLinkListItem.Control = TONE_SAMPLE_COUNT // [11:00] Transfer Size (manual p.577)
@@ -25,7 +25,7 @@ static void Tone_DmaInit(void) {
 
     GPDMA_Init();
     gpdmaConfig.ChannelNum    = 0;                          // Channel 0
-    gpdmaConfig.SrcMemAddr    = (uint32_t)(dacSamples);     // Source memory
+    gpdmaConfig.SrcMemAddr    = (uint32_t)(Samples_DAC);     // Source memory
     gpdmaConfig.DstMemAddr    = 0; // (unused)
     gpdmaConfig.TransferSize  = TONE_SAMPLE_COUNT;          // Transfer size
     gpdmaConfig.TransferWidth = 0; // (unused)
@@ -36,68 +36,67 @@ static void Tone_DmaInit(void) {
     GPDMA_Setup(&gpdmaConfig); // Setup channel with given parameter
 }
 
-static void Tone_GenerateSineWave(float volume) {
-    uint32_t i;
-    float baseLevel = 512.0f * (float)volume / 100.0f;
-    for (i = 0; i < TONE_SAMPLE_COUNT; i++) {
-        dacSamples[i] = baseLevel + baseLevel * sinf((float)i * (2.0f * M_PI / TONE_SAMPLE_COUNT));
-        dacSamples[i] = DAC_VALUE(dacSamples[i]);
+static void Tone_GenerateSineWave(int voulme) {
+    if (voulme > 100) voulme = 100;
+    if (voulme < 1)   voulme = 1;
+    for (int i = 0; i < TONE_SAMPLE_COUNT; i++) {
+        Samples_Sine[i] = 512.0f + 512.0f * sinf((float)i * (2.0f * M_PI / TONE_SAMPLE_COUNT));
+        Samples_Sine[i] = (uint32_t)((float)Samples_Sine[i] * (float)voulme / 100.0f);
     }
 }
 
-void Tone_Init(uint8_t volume) {
-    AF_ConfigPin(GPIO_PORT_0, PINSEL_PIN_26, PINSEL_FUNC_2); //	P0.26 Pin AF Configuration (DAC)
-    DAC_Init(LPC_DAC);
-    Tone_Volume = volume;
-
-    // Tone_GenerateSineWave();
-    Tone_DmaInit();
-	GPDMA_ChannelCmd(0, ENABLE);
+void Tone_SetVolume(int volume) {
+    if (volume > 100) volume = 100;
+    if (volume < 1)   volume = 1;
+    for (int i = 0; i < TONE_SAMPLE_COUNT; i++) {
+        Samples_DAC[i] = DAC_VALUE((int)((float)Samples_Sine[i] * (float)volume / 100.0f));
+    }
 }
 
-void Tone_Play(uint32_t frequency) {
+void Tone_Init(int masterVolume) {
+    AF_ConfigPin(GPIO_PORT_0, PINSEL_PIN_26, PINSEL_FUNC_2); //	P0.26 Pin AF Configuration (DAC)
+    Tone_DmaInit();
+    Tone_GenerateSineWave(masterVolume);
+    Tone_SetVolume(100);
+    DAC_Init(LPC_DAC);
+    GPDMA_ChannelCmd(0, ENABLE);
+}
+
+void Tone_Play(int frequency) {
     uint32_t clockPerSample = (float)(SystemCoreClock / 4) / (float)frequency / TONE_SAMPLE_COUNT;
 
-    DAC_CONVERTER_CFG_Type dacConfig;
-    dacConfig.CNT_ENA = SET; //	Auto Reload
-    dacConfig.DMA_ENA = SET; // Use DMA in DAC
-
-    float r = 0.2f;
-    if (frequency <= NOTE_E4)
-        r = 1.0f;
-    else if (frequency <= NOTE_A4)
-        r = 0.5f;
-    Tone_GenerateSineWave((float)Tone_Volume * r);
     Tone_Stop();
-    DAC_SetDMATimeOut(LPC_DAC, clockPerSample - 1);
-    DAC_ConfigDAConverterControl(LPC_DAC, &dacConfig);
+    if (frequency <= NOTE_E4)
+        Tone_SetVolume(100);
+    else if (frequency <= NOTE_A4)
+        Tone_SetVolume(50);
+    else
+        Tone_SetVolume(30);
 
-    
-    Tone_State = TRUE;
+    DAC_CONVERTER_CFG_Type DAC_Config;
+    DAC_Config.CNT_ENA = SET; // Auto Reload
+    DAC_Config.DMA_ENA = SET; // Use DMA in DAC
+    DAC_SetDMATimeOut(LPC_DAC, clockPerSample - 1);
+    DAC_ConfigDAConverterControl(LPC_DAC, &DAC_Config);
+    Tone_State = 1;
 }
 
 void Tone_Stop(void) {
     if (Tone_State) {
-            DAC_CONVERTER_CFG_Type dacConfig;
-    dacConfig.CNT_ENA = RESET; //	Auto Reload
-    dacConfig.DMA_ENA = RESET; // Use DMA in DAC
-			DAC_ConfigDAConverterControl(LPC_DAC, &dacConfig);
+        DAC_CONVERTER_CFG_Type DAC_Config;
+        DAC_Config.CNT_ENA = RESET;
+        DAC_Config.DMA_ENA = RESET;
+        DAC_ConfigDAConverterControl(LPC_DAC, &DAC_Config);
     }
-    Tone_State = false;
+    Tone_State = 0;
 }
 
-bool Tone_IsPlaying(void) {
+int Tone_IsPlaying(void) {
     return Tone_State;
 }
 
-void Tone_Test(uint32_t millis) {
-    Tone_Play(NOTE_C4); DelayMs(millis); // 도
-    Tone_Play(NOTE_D4); DelayMs(millis); // 레
-    Tone_Play(NOTE_E4); DelayMs(millis); // 미
-    Tone_Play(NOTE_F4); DelayMs(millis); // 파
-    Tone_Play(NOTE_G4); DelayMs(millis); // 솔
-    Tone_Play(NOTE_A4); DelayMs(millis); // 라
-    Tone_Play(NOTE_B4); DelayMs(millis); // 시
-    Tone_Play(NOTE_C5); DelayMs(millis); // 도
-    Tone_Stop();
+#pragma O0
+void Tone_Delay(volatile uint32_t millis) {
+    millis = SystemCoreClock / 4000 * millis;
+    while(millis--);
 }
--- Source/Tone.h
+++ Source/Tone.h
@@ -4,10 +4,11 @@
 #include "Utility.h"
 #include "../Resource/Note.h"
 
-void Tone_Init     (uint8_t  volume);
-void Tone_Play     (uint32_t frequency);
+void Tone_Init     (int masterVolume);
+void Tone_SetVolume(int volume);
+void Tone_Play     (int frequency);
 void Tone_Stop     (void);
-bool Tone_IsPlaying(void);
-void Tone_Test     (uint32_t millis);
+int  Tone_IsPlaying(void);
+void Tone_Delay    (volatile uint32_t millis);
 
 #endif
